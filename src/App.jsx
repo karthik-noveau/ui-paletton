ff69b4482bfa23d88f352dffba7c6ad666cbdcc4
karthik <karthik5112001@gmail.com>
2025-08-02 23:58:54 +0530

    v fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter } from "react-router";
 import { HomePage } from "./pages/home";
 
-import "./app.css";
-
 export default function App() {
   return (
     <BrowserRouter>
