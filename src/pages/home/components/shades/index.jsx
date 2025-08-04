1f4483261f9713537df0c4d1c2db6b667f3af922
karthik <karthik5112001@gmail.com>
2025-08-04 14:59:16 +0530

    shades fix

--- src/pages/home/components/shades/index.jsx
+++ src/pages/home/components/shades/index.jsx
@@ -107,5 +107,5 @@ const getContrastColor = (hex) => {
   // Calculate luminance
   const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
 
-  return luminance > 120 ? "#000000" : "#ffffff";
+  return luminance > 140 ? "#000000" : "#ffffff";
 };
